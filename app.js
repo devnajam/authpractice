58adaa771ab1a0ed1dded119b6ddc19776e26df2
angrynajam <najamiftikhar2020@gmail.com>
2020-04-10 10:58:30 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
